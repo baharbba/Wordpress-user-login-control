5328e7becd535748ae4539f0a036a9c5d081598d
baharbba <94792841+baharbba@users.noreply.github.com>
2022-09-21 02:20:09 +0300

    Update function.php

--- php1/function.php
+++ php1/function.php
@@ -13,12 +13,12 @@ add_action( 'edit_user_profile', 'bks_add_person_reg_number_field' );
 add_action( 'show_user_profile', 'bks_add_person_reg_number_field' );
 
 
-function bks_add_student_reg_number_field( $user ) {
+function bks_add_person_reg_number_field( $user ) {
     ?>
     <h3><?php _e("Extra profile information", "blank"); ?></h3>
     <table class="form-table">
         <tr>
-            <th><label for="student_reg_number"><?php _e("Person Registration Number"); ?></label></th>
+            <th><label for="person_reg_number"><?php _e("Person Registration Number"); ?></label></th>
             <td>
                 <?php if (is_object($user)) { ?>
                     <input type="text" name="person_reg_number" id="person_reg_number" value="<?php echo esc_attr( get_the_author_meta( 'person_reg_number', $user->ID )); ?>" class="regular-text" readonly disabled="disabled"/><br />
